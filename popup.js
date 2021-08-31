14aacb0070ac142dea8744e403fa47adedf593ef
Marcel Walk <walk.marcel.97@googlemail.com>
2021-08-31 23:58:55 +0200

    Added UI
    * Added basic popup
    * Added icons
    * Added index for displaying notes

---
+++ popup.js
@@ -0,0 +1,10 @@
+let showNotes = document.getElementById("showNotes");
+
+showNotes.addEventListener("click", () => {
+    extension_id = chrome.runtime.id
+    let url = "chrome-extension://" + extension_id + "/index.html"
+    window.open(url, '_blank').focus();
+    chrome.tabs.create({
+        url: url
+    });
+});
