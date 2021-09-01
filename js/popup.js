32eb45b151f3229b1c06e0e408246e4963ddc8c1
Marcel Walk <walk.marcel.97@googlemail.com>
2021-09-01 17:50:39 +0200

    Restructured project & added index.js

--- js/popup.js
+++ js/popup.js
@@ -2,7 +2,7 @@ let showNotes = document.getElementById("showNotes");
 
 showNotes.addEventListener("click", () => {
     extension_id = chrome.runtime.id
-    let url = "chrome-extension://" + extension_id + "/index.html"
+    let url = "chrome-extension://" + extension_id + "/html/index.html"
     window.open(url, '_blank').focus();
     chrome.tabs.create({
         url: url
