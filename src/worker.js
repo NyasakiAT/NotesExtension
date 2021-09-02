7c489bfe67b040ccaef0f13e7555d6bf6a11d81a
Marcel Walk <walk.marcel.97@googlemail.com>
2021-09-02 19:14:55 +0200

    Added notification on note save
    * Added notifications permission to manifest
    * Added notification call in save_note
    
    Resolving #2

--- src/worker.js
+++ src/worker.js
@@ -21,4 +21,11 @@ function save_note(info, tab) {
   chrome.storage.sync.set(data, function () {
     console.log('Note ' + note_id + ' saved')
   })
+
+  chrome.notifications.create({
+    type: "basic",
+    title: "Note Extension",
+    message: "Note saved sucessfully",
+    iconUrl: "./images/notes-icon_128.png"
+  });
 }
